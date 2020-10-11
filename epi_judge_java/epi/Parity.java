13705840ac4acbd4de2d9a6c7d0010fd201dffe8
ST20911 <jonathan.sawyer@gmail.com>
2020-10-11 12:43:41 +0900

    as

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,15 +4,19 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short result = 0;
+    while(x > 0) {
+      result = (short) (result ^ 1);
+      // clear the lowest set bit
+      x = x & (x - 1);
+    }
+    return result;
   }
 
   public static void main(String[] args) {
-    System.exit(
         GenericTest
             .runFromAnnotations(args, "Parity.java",
                                 new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
+            .ordinal();
   }
 }
--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -1,18 +1,27 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class SwapBits {
-  @EpiTest(testDataFile = "swap_bits.tsv")
-  public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
+import java.io.Console;
+
+public class SwapBits {
+    @EpiTest(testDataFile = "swap_bits.tsv")
+    public static long swapBits(long x, int i, int j) {
+        long bitSet1 = (x >>> i) & 1;
+        long bitSet2 = (x >>> j) & 1;
+        if (bitSet1 != bitSet2) {
+          long mask = 1L << i | 1L << j;
+          x ^= mask;
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+      System.exit(
         GenericTest
             .runFromAnnotations(args, "SwapBits.java",
                                 new Object() {}.getClass().getEnclosingClass())
             .ordinal());
-  }
+    }
 }
